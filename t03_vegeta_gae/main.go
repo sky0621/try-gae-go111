24f18c0f3472a49bac8825b5cf87f09798e7e9c2
sky0621 <sky14work@gmail.com>
2019-05-12 02:31:23 +0900

    continue

--- t03_vegeta_gae/main.go
+++ t03_vegeta_gae/main.go
@@ -2,69 +2,80 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"net/http"
 	"os"
-	"time"
-
-	"github.com/google/uuid"
 
 	"github.com/jinzhu/gorm"
+
+	"github.com/google/uuid"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo"
 )
 
 func main() {
-	// --------------------------------------------------------------
+	// ローカル環境での実行時は、環境変数にセットしておく。例：「export IS_LOCAL='yes'」
+	isLocal := os.Getenv("IS_LOCAL") != ""
+
+	// Cloud SQL (ローカルでは MySQL) への接続
+	db, closeDBFunc, err := connectDB(isLocal)
+	if err != nil {
+		os.Exit(-1)
+	}
+	defer closeDBFunc(db)
+
+	// WebAPIサーバ起動
+	e := echo.New()
+	routing(e, db)
+
+	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
+	fmt.Printf("[GWFGS] PORT: %s\n", port)
+	if isLocal {
+		port = ":8080"
+	}
+	e.Logger.Fatal(e.Start(port))
+}
+
+func createDatasource(isLocal bool) string {
+	if isLocal {
+		return "testuser:testpass@tcp(localhost:3306)/testdb?charset=utf8&parseTime=True&loc=Local"
+	}
+
 	// GCP - CloudSQL への接続情報は環境変数から取得
-	// --------------------------------------------------------------
 	var (
 		connectionName = os.Getenv("CLOUDSQL_CONNECTION_NAME")
 		user           = os.Getenv("CLOUDSQL_USER")
 		password       = os.Getenv("CLOUDSQL_PASSWORD")
 		database       = os.Getenv("CLOUDSQL_DATABASE")
 	)
-	dataSource := fmt.Sprintf("%s:%s@unix(/cloudsql/%s)/%s?parseTime=True", user, password, connectionName, database)
 
-	// ローカル環境（docker-composeでMySQLを起動）での動作確認用
-	isLocal := os.Getenv("IS_LOCAL") != ""
-	if isLocal {
-		dataSource = "testuser:testpass@tcp(localhost:3306)/testdb?charset=utf8&parseTime=True&loc=Local"
+	return fmt.Sprintf("%s:%s@unix(/cloudsql/%s)/%s?parseTime=True", user, password, connectionName, database)
+}
+
+type closeDB func(db *gorm.DB)
+
+func closeDBFunc(db *gorm.DB) {
+	if db == nil {
+		return
 	}
+	_ = db.Close()
+}
 
-	// --------------------------------------------------------------
-	// GCP - CloudSQL へ接続
-	// --------------------------------------------------------------
-	db, err := gorm.Open("mysql", dataSource)
+func connectDB(isLocal bool) (*gorm.DB, closeDB, error) {
+	db, err := gorm.Open("mysql", createDatasource(isLocal))
 	if err != nil {
-		panic(err)
+		return nil, nil, err
 	}
-	defer func() {
-		if db != nil {
-			err := db.Close()
-			if err != nil {
-				panic(err)
-			}
-		}
-	}()
 
 	db.LogMode(true)
 	if err := db.DB().Ping(); err != nil {
-		panic(err)
+		return nil, nil, err
 	}
 
-	// チューニングポイント
-	db.DB().SetMaxIdleConns(100)
-	db.DB().SetMaxOpenConns(100)
+	return db, closeDBFunc, nil
+}
 
-	rand.Seed(time.Now().UnixNano())
-
-	e := echo.New()
-
-	// --------------------------------------------------------------
-	// 各種WebAPIの定義
-	// --------------------------------------------------------------
+func routing(e *echo.Echo, db *gorm.DB) {
 	// ユーザー登録
 	e.POST("/users", func(c echo.Context) error {
 		id := uuid.New().String()
@@ -87,13 +98,6 @@ func main() {
 		}
 		return c.JSON(http.StatusOK, u)
 	})
-
-	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
-	fmt.Printf("[GWFGS] PORT: %s\n", port)
-	if isLocal {
-		port = ":8080"
-	}
-	e.Logger.Fatal(e.Start(port))
 }
 
 type User struct {
